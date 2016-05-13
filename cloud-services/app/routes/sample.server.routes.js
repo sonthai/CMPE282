445b2c323064bde4c4cd2204188079ef2a302537
Ubuntu <ubuntu@ip-10-0-1-42.us-west-2.compute.internal>
2016-05-13 07:01:15 +0000

    Remove old project

--- cloud-services/app/routes/sample.server.routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-	// Routing logic   
-	var samples = require('../../app/controllers/sample.server.controller');
-	app.route('/sample-test').get(samples.test);
-};
--- cloud-services/public/modules/core/controllers/test.client.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-angular.module('core').controller('TestController', ['$scope',
-	function($scope) {
-		// This provides Authentication context.
-		$scope.helloMsg = "Hello world binding";
-	}
-]);
